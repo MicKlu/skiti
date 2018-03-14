dc683b83e686187b0c38584ec5a9fd45205b7424
MGRINZ <mgrinz.official@o2.pl>
2018-03-15 00:01:45 +0100

    navbar: bez wyszukiwarki

--- header.php
+++ header.php
@@ -7,7 +7,29 @@
 		<meta name="keywords" content="" />
 		<meta name="author" content="" />
 		<link rel="stylesheet" href="css/main.css" />
+		<link rel="icon" href="img/favicon.png" />
 		<script src="scripts/jquery-3.3.1.min.js"></script>
 		<script src="scripts/main.js"></script>
 	</head>
 	<body>
+		<nav id="navbar">
+			<div class="left-pane">
+				<a class="brand" href="/">
+					<img class="logo" src="img/logo.svg" /> dsa
+				</a>
+			</div>
+			<div class="right-pane">
+				<ul class="navbar-menu">
+					<li><a href="#"><img class="navbar-user-avatar" src="img/logo.svg" />{Imię i nazwisko}</a></li>
+					<li><a href="#">Ustawienia</a></li>
+					<li><a href="#">Wyloguj</a></li>
+				</ul>
+				<!--
+				<form action="" method="post">
+					<input type="text" id="email" placeholder="Email" />
+					<input type="password" id="password" placeholder="Hasło" />
+					<input type="submit" value="Zaloguj" />
+				</form>
+				-->
+			</div>
+		</nav>
