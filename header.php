8c9e6ef7c8dfcc8cd2558b60d8e32759e78b7da8
MGRINZ <mgrinz.official@o2.pl>
2018-03-15 13:59:24 +0100

    navbar: wyszukiwarka; font-awesome

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" href="css/main.css" />
 		<link rel="icon" href="img/favicon.png" />
 		<script src="scripts/jquery-3.3.1.min.js"></script>
+		<script src="scripts/fontawesome-all.min.js"></script>
 		<script src="scripts/main.js"></script>
 	</head>
 	<body>
@@ -17,19 +18,25 @@
 				<a class="brand" href="/">
 					<img class="logo" src="img/logo.svg" /> dsa
 				</a>
+				<!-- Zalogowany -->
+				<form class="input-search" action="">
+					<input type="text" placeholder="Wyszukaj" size="48" />
+					<button type="submit"><i class="fas fa-search"></i></button>
+				</form>
 			</div>
 			<div class="right-pane">
+				<!-- Zalogowany -->
 				<ul class="navbar-menu">
 					<li><a href="#"><img class="navbar-user-avatar" src="img/logo.svg" />{Imię i nazwisko}</a></li>
 					<li><a href="#">Ustawienia</a></li>
 					<li><a href="#">Wyloguj</a></li>
 				</ul>
-				<!--
+				<!-- Nie zalogowany -->
 				<form action="" method="post">
 					<input type="text" id="email" placeholder="Email" />
 					<input type="password" id="password" placeholder="Hasło" />
 					<input type="submit" value="Zaloguj" />
 				</form>
-				-->
+				
 			</div>
 		</nav>
--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php require "header.php" ?>
 <section id="jumbotron">
-<!-- <img class="logo" src="img/logo.svg" /> -->
+<img class="logo" src="img/logo.svg" />
 <h1>dsa</h1>
 <h2>Kolejny portal społecznościowy</h2>
 <button>Zarejestruj się</button>
 </section>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec iaculis massa. Etiam tristique scelerisque diam interdum sagittis. Vivamus eu accumsan augue. Duis a dolor laoreet, pharetra neque ornare, elementum purus. Sed accumsan, justo vitae lacinia posuere, ante urna commodo diam, et placerat ligula lorem ullamcorper enim. Phasellus laoreet laoreet tortor, in vehicula eros mollis sed. Aenean nec odio lacinia nunc euismod lacinia. Sed mattis fermentum porttitor.
-
-</p>
 <?php require "footer.php" ?>
