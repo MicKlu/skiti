4bf65827bd670bd05f79a840d97480f6c7d94b3a
MGRINZ <mgrinz.official@o2.pl>
2018-03-18 21:22:58 +0100

    Panele pod jumbotronem

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php require "header.php" ?>
-<section id="jumbotron">
-<img class="logo" src="img/logo.svg" />
-<h1>dsa</h1>
-<h2>Kolejny portal społecznościowy</h2>
-<button>Zarejestruj się</button>
-</section>
+<main>
+	<section class="jumbotron">
+		<img class="logo" src="img/logo.svg" />
+		<h1>dsa</h1>
+		<h2>Kolejny portal społecznościowy</h2>
+		<button>Zarejestruj się</button>
+	</section>
+	<section class="container" id="features">
+		<div class="row">
+			<div class="col-4 feature">
+				<header>
+					<i class="fas fa-users feature-icon"></i>
+					<h4>Kontaktuj się ze znajomymi</h4>
+				</header>
+				<p>Wyszukuj i dodawaj znajomych, aby pozostawać z nimi w kontakcie. Zostawiaj wiadomości na tablicach, aby szybko mogli je odczytać.</p>
+			</div>
+			<div class="col-4 feature">
+				<header>
+					<i class="far fa-images feature-icon"></i>
+					<h4>Udostępniaj zdjęcia</h4>
+				</header>
+				<p>Obecnie każdy portal społecznościowy umożliwia udostępnianie zdjęć. Czemu ten miałby być gorszy? Dodawaj zdjęcia i wyróżniaj je na profilu.</p>
+			</div>
+			<div class="col-4 feature">
+				<header>
+					<i class="fas fa-check feature-icon"></i>
+					<h4>Prostota obsługi</h4>
+				</header>
+				<p>Nasz serwis powstał z myślą o jego użytkownikach. Najważniejsze funkcje znajdują się w dobrze widocznych miejscach.</p>
+			</div>
+		</div>
+	</section>
+	<section class="container" id="register">
+		
+	</section>
+</main>
 <?php require "footer.php" ?>
