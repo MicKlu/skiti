c0e2df654fdb9e2c6ee45efb3fc86da187f59951
MGRINZ <mgrinz.official@o2.pl>
2018-03-28 20:46:20 +0200

    Strona zdjęć: Wyświetlanie kafelków (dopasowywanie wielkości), panel komentarzy

---
+++ profile_photos.php
@@ -0,0 +1,231 @@
+<section id="profile-photos">
+	<div id="profile-photos-menu">
+		<button class="button-primary">Dodaj zdjęcie</button>
+	</div>
+	<div class="row">
+		<div class="col-1">
+			<div class="profile-image-box">
+				<div class="profile-image-wrapper">
+					<img src="img/placeholder.png" />
+				</div>
+				<div class="profile-image-panel">
+					<div class="profile-image-info">
+						<h3>{Nazwa pliku}</h3>
+						<p>{Podpis}</p>
+					</div>
+					<div class="profile-image-buttons">
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-up"></i> (1)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-down"></i> (123)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-comments"></i> (9)</a>
+						<!-- Tylko dla właściciela -->
+						<a class="button button-light button-primary" href="#"><i class="fas fa-pencil-alt"></i></a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-2">
+			<div class="profile-image-box">
+				<div class="profile-image-wrapper">
+					<img src="img/placeholder.png"/>
+				</div>
+				<div class="profile-image-panel">
+					<div class="profile-image-info">
+						<h3>{Nazwa pliku}</h3>
+						<p>{Podpis}</p>
+					</div>
+					<div class="profile-image-buttons">
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-up"></i> (1)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-down"></i> (123)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-comments"></i> (9)</a>
+						<!-- Tylko dla właściciela -->
+						<a class="button button-light button-primary" href="#"><i class="fas fa-pencil-alt"></i></a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-1">
+			<div class="profile-image-box">
+				<div class="profile-image-wrapper">
+					<img src="img/favicon.png"/>
+				</div>
+				<div class="profile-image-panel">
+					<div class="profile-image-info">
+						<h3>{Nazwa pliku}</h3>
+						<p>{Podpis}</p>
+					</div>
+					<div class="profile-image-buttons">
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-up"></i> (1)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-down"></i> (123)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-comments"></i> (9)</a>
+						<!-- Tylko dla właściciela -->
+						<a class="button button-light button-primary" href="#"><i class="fas fa-pencil-alt"></i></a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-times"></i></a>
+					</div>
+						<!-- Dynamicznie generowane przez JS -->
+					<div class="profile-image-comment-box">
+						<div class="profile-image-comment-inner-box">
+							<div class="profile-image-comments">
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								<div class="profile-comment">
+									<div class="profile-comment-avatar"><img class="profile-avatar-mini" src="img/placeholder.png" /></div>
+									<div class="profile-comment-content">
+										<h6>{Imię} {Nazwisko}</h6>
+										<p>{Komentarz}</p>
+									</div>
+								</div>
+								
+							</div>
+							<div class="profile-image-comment-form">
+								<form>
+									<textarea></textarea>
+									<button class="button-light button-primary">Wyślij komentarz</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-1">
+			<div class="profile-image-box">
+				<div class="profile-image-wrapper">
+					<img src="img/placeholder.png"/>
+				</div>
+				<div class="profile-image-panel">
+					<div class="profile-image-info">
+						<h3>{Nazwa pliku}</h3>
+						<p>{Podpis}</p>
+					</div>
+					<div class="profile-image-buttons">
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-up"></i> (1)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-down"></i> (123)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-comments"></i> (9)</a>
+						<!-- Tylko dla właściciela -->
+						<a class="button button-light button-primary" href="#"><i class="fas fa-pencil-alt"></i></a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-1">
+			<div class="profile-image-box">
+				<div class="profile-image-wrapper">
+					<img src="img/placeholder.png"/>
+				</div>
+				<div class="profile-image-panel">
+					<div class="profile-image-info">
+						<h3>{Nazwa pliku}</h3>
+						<p>{Podpis}</p>
+					</div>
+					<div class="profile-image-buttons">
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-up"></i> (1)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-down"></i> (123)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-comments"></i> (9)</a>
+						<!-- Tylko dla właściciela -->
+						<a class="button button-light button-primary" href="#"><i class="fas fa-pencil-alt"></i></a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-1">
+			<div class="profile-image-box">
+				<div class="profile-image-wrapper">
+					<img src="img/placeholder.png"/>
+				</div>
+				<div class="profile-image-panel">
+					<div class="profile-image-info">
+						<h3>{Nazwa pliku}</h3>
+						<p>{Podpis}</p>
+					</div>
+					<div class="profile-image-buttons">
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-up"></i> (1)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-down"></i> (123)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-comments"></i> (9)</a>
+						<!-- Tylko dla właściciela -->
+						<a class="button button-light button-primary" href="#"><i class="fas fa-pencil-alt"></i></a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-2">
+			<div class="profile-image-box">
+				<div class="profile-image-wrapper">
+					<img src="img/placeholder.png"/>
+				</div>
+				<div class="profile-image-panel">
+					<div class="profile-image-info">
+						<h3>{Nazwa pliku}</h3>
+						<p>{Podpis}</p>
+					</div>
+					<div class="profile-image-buttons">
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-up"></i> (1)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-thumbs-down"></i> (123)</a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-comments"></i> (9)</a>
+						<!-- Tylko dla właściciela -->
+						<a class="button button-light button-primary" href="#"><i class="fas fa-pencil-alt"></i></a>
+						<a class="button button-light button-primary" href="#"><i class="fas fa-times"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- itd -->
+</section>
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,55 @@
 $(function () {
-	
+	$(window).on("resize", normalizeProfileImages)
+	normalizeProfileImages();
 })
+
+function normalizeProfileImages() {	
+	//Środkowanie małych obrazów
+	$("#profile-photos .profile-image-wrapper > img").each(function () {
+		var image = $(this);
+		var imgHeight = image.height();
+		if(imgHeight < 300) {
+			var offset = (150 - imgHeight / 2);	//(300 / 2) - imgHeight / 2
+			image.css({top: offset + "px"});	//Przesuwamy
+		}
+	});
+	
+	//Środkowanie dużych obrazów i wyrównanie długich opisów
+	$("#profile-photos .row").each(function () {
+		var row = $(this);
+		var imgWrappers = row.find(".profile-image-wrapper");
+		
+		if(imgWrappers.length == 2) {	//Jeśli w rzędzie są dwa obrazy...
+			var images = imgWrappers.children("img");
+			var captions = row.find(".profile-image-info");
+			var imgWrappersHeights = [];
+			var imgsHeights = [];
+			var captionsHeights = [];
+			imgWrappersHeights[0] = imgWrappers.eq(0).height();
+			imgWrappersHeights[1] = imgWrappers.eq(1).height();
+			imgsHeights[0] = images.eq(0).height();
+			imgsHeights[1] = images.eq(1).height();
+			captionsHeights[0] = captions.eq(0).height();
+			captionsHeights[1] = captions.eq(1).height();
+			
+			//Skalujemy większy obraz do wysokości mniejszego
+			if(imgWrappersHeights[0] > imgWrappersHeights[1]) {			//Pierwszy obraz za duży
+				var offset = (imgWrappersHeights[1] / 2 - imgsHeights[0] / 2);	//newWrapperHeight / 2 - imgHeight / 2
+				imgWrappers.eq(0).height(imgWrappersHeights[1]);	//Skalujemy	
+				images.eq(0).css({top: offset + "px"});	//Przesuwamy
+			}
+			else if(imgWrappersHeights[0] < imgWrappersHeights[1]) {	//Drugi obraz za duży
+				var offset = (imgWrappersHeights[0] / 2 - imgsHeights[1] / 2);	//newWrapperHeight / 2 - imgHeight / 2
+				imgWrappers.eq(1).height(imgWrappersHeights[0]);	//Skalujemy
+				images.eq(1).css({top: offset + "px"});	//Przesuwamy
+			}
+			
+			//Skalujemy mniejszy opis do wysokości większego
+			if(captionsHeights[0] > captionsHeights[1])			//Pierwszy opis
+				captions.eq(1).height(captionsHeights[0]);	//Skalujemy
+			else if(captionsHeights[0] < captionsHeights[1])	//Drugi opis
+				captions.eq(0).height(captionsHeights[1]);	//Skalujemy
+			
+		}
+	});
+}
